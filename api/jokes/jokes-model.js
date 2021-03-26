c562d72bbed72ba549ba79cd25f8dfa1a9eb8e9b
Stephanie <stephanie.enciso13@gmail.com>
2021-03-26 10:30:04 -0500

    Initial commit

---
+++ api/jokes/jokes-model.js
@@ -0,0 +1,19 @@
+const db = require('../../data/dbConfig')
+
+module.exports = {
+    findBy,
+    findById,
+    insert
+}
+
+function findBy(filter) {
+
+}
+
+function findById(id) {
+
+}
+
+async function insert(user) {
+    
+}
