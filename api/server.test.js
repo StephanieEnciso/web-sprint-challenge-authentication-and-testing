e59b46f666b19449302aa9ea0266984c3fc4c0cf
Stephanie <stephanie.enciso13@gmail.com>
2021-03-26 12:51:41 -0500

    All endpoints are working

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,29 @@
-// Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+const user = { username: 'abc', password: 'abc123'}
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async (done) => {
+  await db.destroy()
+  done()
+})
+
+describe('[POST] /api/auth/register', () => {
+  beforeEach(async () => {
+    await db('users').truncate()
+  })
+  it('responds with correct error status code if username or password is missing', async () => {
+    let res = await await request(server).post('/api/auth/register').send({})
+    expect(res.statusCode).toEqual(400)
+  })
 })
